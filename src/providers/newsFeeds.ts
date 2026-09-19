0f6c94e10a8645bd3493a764df3e928810d4ed54
Jaguar023-dev <derrickithicia8@gmail.com>
2026-09-19 13:15:11 +0300

    Create src/providers/newsFeeds.ts

---
+++ src/providers/newsFeeds.ts
@@ -0,0 +1,10 @@
+export const NEWS_FEEDS: Record<string, string> = {
+  kenyanews: 'https://allafrica.com/tools/headlines/rdf/kenya/headlines.rdf',
+  africanews: 'https://allafrica.com/tools/headlines/rdf/africa/headlines.rdf',
+  worldnews: 'https://feeds.bbci.co.uk/news/world/rss.xml',
+  technews: 'https://feeds.arstechnica.com/arstechnica/index',
+  politicsnews: 'https://feeds.bbci.co.uk/news/politics/rss.xml',
+  sportsnews: 'https://feeds.bbci.co.uk/sport/rss.xml',
+  headlines: 'https://feeds.bbci.co.uk/news/rss.xml',
+  trending: 'https://trends24.in/kenya/feed.xml',
+};
