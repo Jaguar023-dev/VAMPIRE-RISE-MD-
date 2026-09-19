55f3d72d23bd275af8d72d6dea4ecbf20c1b6e58
Jaguar023-dev <derrickithicia8@gmail.com>
2026-09-19 13:25:14 +0300

    Create src/commands/_index.ts

---
+++ src/commands/_index.ts
@@ -0,0 +1,15 @@
+// Importing each command module registers its commands.
+import './info';
+import './ai';
+import './music';
+import './download';
+import './media';
+import './search';
+import './sports';
+import './fun';
+import './group';
+import './security';
+import './settings';
+import './news';
+import './vampire';
+import './dev';
